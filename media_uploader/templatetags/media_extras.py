4c2d9c6682295e2e608bb08067635e679fbf7a6a
dsatoh20 <dsatoh@GQ2DPW2>
2025-01-15 18:34:42 +0900

    solved an AttributeError

--- media_uploader/templatetags/media_extras.py
+++ media_uploader/templatetags/media_extras.py
@@ -5,18 +5,18 @@ register = template.Library()
 
 @register.simple_tag
 def get_favicon():
-    img = StaticMedia.objects.filter(name='favicon').first().image
-    return img.url if img else ''
+    img = StaticMedia.objects.filter(name='favicon').first()
+    return img.image.url if img else ''
 @register.simple_tag
 def get_logo120():
     img = StaticMedia.objects.filter(name='logo120').first().image
-    return img.url if img else ''
+    return img.image.url if img else ''
 @register.simple_tag
 def get_logo256():
     img = StaticMedia.objects.filter(name='logo256').first().image
-    return img.url if img else ''
+    return img.image.url if img else ''
 @register.simple_tag
 def get_logo512():
     img = StaticMedia.objects.filter(name='logo512').first().image
-    return img.url if img else ''
+    return img.image.url if img else ''
     
