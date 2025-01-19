f0006e251149b81672d7462fa874b2e5e2330682
dsatoh20 <dsatoh@GQ2DPW2>
2025-01-19 23:42:39 +0900

    概要を掲載

--- media_uploader/templatetags/media_extras.py
+++ media_uploader/templatetags/media_extras.py
@@ -20,3 +20,22 @@ def get_logo512():
     img = StaticMedia.objects.filter(name='logo512').first()
     return img.image.url if img else ''
     
+@register.simple_tag
+def get_moodboard():
+    img = StaticMedia.objects.filter(name='moodboard').first()
+    return img.image.url if img else ''
+
+@register.simple_tag
+def get_mockup_home():
+    img = StaticMedia.objects.filter(name='mockup_home').first()
+    return img.image.url if img else ''
+
+@register.simple_tag
+def get_mockup_record():
+    img = StaticMedia.objects.filter(name='mockup_record').first()
+    return img.image.url if img else ''
+
+@register.simple_tag
+def get_mockup_post():
+    img = StaticMedia.objects.filter(name='mockup_post').first()
+    return img.image.url if img else ''
