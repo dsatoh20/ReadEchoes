a0d4620e633905820254cfc1e25bb8e51ee74585
dsatoh20 <dsatoh20@gmail.com>
2026-01-30 15:59:53 +0900

    publicチーム所属レコードのみ表示

--- records/sitemaps.py
+++ records/sitemaps.py
@@ -1,12 +1,15 @@
 from django.contrib.sitemaps import Sitemap
-from .models import *
+from .models import Book
+from accounts.models import Team
 
 class RecordSitemap(Sitemap):
     changefreq = "weekly"
     priority = 0.8
 
     def items(self):
-        return Book.objects.distinct().order_by('-id')
+        pub_teams = Team.objects.filter(public=True).distinct()
+        pub_items = Book.objects.filter(team__in=pub_teams).distinct()
+        return pub_items
 
     def location(self, obj):
         return f"/record/{obj.id}"
